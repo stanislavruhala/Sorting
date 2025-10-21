10494422c307bac70a7afa01607bcb0e3ef8c623
stanislavruhala <stanislav.ya.ruhala@ukd.edu.ua>
2025-10-21 10:52:39 +0300

    Update and rename README.md to sorting.js

---
+++ sorting.js
@@ -0,0 +1,15 @@
+function sorting(arrayToSort) {
+    for (let j = 0; j < arrayToSort.length - 1; j++) {
+        for (let i = j + 1; i < arrayToSort.length; i++) {
+            if (arrayToSort[j] >= arrayToSort[i]) {
+                let temp = arrayToSort[j];
+                arrayToSort[j] = arrayToSort[i];
+                arrayToSort[i] = temp;
+            }
+        }
+    }
+    return arrayToSort;
+}
+
+const sortedarrayToSort = sorting([2, 5, 2, 1, -9]);
+console.log({ sortedarrayToSort });# Sorting
